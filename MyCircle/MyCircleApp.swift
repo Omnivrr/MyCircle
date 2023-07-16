6e568c2e3ca22d90fe9041256ce6954a24c9904c
Omnivrr <Omnivrr@gmail.com>
2023-07-16 18:05:26 +0100

    Initial Commit

---
+++ MyCircle/MyCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCircleApp.swift
+//  MyCircle
+//
+//  Created by Bukhari Sani on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
